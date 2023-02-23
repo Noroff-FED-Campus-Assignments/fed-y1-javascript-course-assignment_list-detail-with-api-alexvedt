f758e5b9fcdbfd9ac2150958add37fd72895c860
alexvedt <alexvedt@gmail.com>
2023-02-23 12:27:54 +0100

    1

---
+++ main.js
@@ -0,0 +1,63 @@
+const resultsContainer = document.querySelector("#results");
+
+const url = "https://rickandmortyapi.com/api/character";
+
+const searchBar = document.querySelector("#js-search-bar");
+
+searchBar.addEventListener("keyup", (event) => {
+  const searchString = event.target.value.trim().toLowerCase();
+  console.log("searchString>>>>>", searchString);
+  fetchCharacters(searchString);
+});
+
+async function fetchCharacters(searchString = "") {
+  try {
+    const response = await fetch(url);
+    const json = await response.json();
+    console.log(json);
+
+    resultsContainer.innerHTML = "";
+
+    const ppls = json.results;
+    const filteredCharacters = filterCharacters(ppls, searchString);
+    const characters = filteredCharacters;
+
+    for (let i = 0; i < filteredCharacters.length; i++) {
+      resultsContainer.innerHTML += `
+      <a href="/detailspage.html?id=${characters[i].id}" class="card">
+          <div class="details">
+              <h4 class="name">${characters[i].name}</h4>
+              <div class="image" style="background-image: url(${characters[i].image});"></div>
+                <p class="status">Status: ${characters[i].status}</p>
+                <p class="species">Species: ${characters[i].species}</p>
+                <p class="origin">Origin: ${characters[i].origin.name}</p>
+          </div>
+      </div>
+      </a>
+
+      `;
+    }
+  } catch (error) {
+    console.log(error);
+    resultsContainer.innerHTML = `<p>Something wrong happened.. => ${error}</p>`;
+  }
+}
+try {
+  console.log("This will run");
+} finally {
+  console.log("This will always run");
+}
+
+fetchCharacters();
+
+// helper functions
+function filterCharacters(characters, searchString) {
+  return characters.filter(function (character) {
+    return (
+      character.name.toLowerCase().includes(searchString) ||
+      character.type.toLowerCase().includes(searchString) ||
+      character.species.toLowerCase().includes(searchString) ||
+      character.status.toLowerCase().includes(searchString)
+    );
+  });
+}
